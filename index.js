3de824b601d11ab8d9292302a87b913ae60c26f5
Shuvam Adhikari <shuvamlim234@gmail.com>
2023-08-02 17:02:16 +0545

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+// // // console.log("ram");
+// // // var ram="hari";
+// // // ram="shyam";
+// // // console.log(ram);
+
+
+// // // let u=45;
+// // // console.log(u);
+// // // u=44;
+// // // console.log(u);
+
+// // // const z=7;
+// // // console.log(z);
+// // alert("user name ans password is wrong");
+// // const name = prompt("Enter your name");
+// // const pw = prompt("Enter your pw");
+
+// // if (name === "shuvam" && pw === "123") {
+// //     alert ("welccome t our website");
+// // }
+// // prompt("Enter your name");
+// // alert("You have a nice name");
+// // confirm("Are you 18 years old");
+
+// const name = prompt("Enter your name");
+
+
