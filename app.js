0a692332190a013ade27f242059062870a55ac67
Shuvam Adhikari <shuvamlim234@gmail.com>
2023-08-07 18:04:25 +0545

    My module

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const date = require("./myModule");
 
 const myFunction = (req, res) => {
     const dateTime = date();
-res.send("hello world");
+    res.end(`${dateTime}`);
 };
-http.createServer(myFunction).listen(3000);
+http.createServer(myFunction).listen(4000);
 console.log("App is running on port 3000");
