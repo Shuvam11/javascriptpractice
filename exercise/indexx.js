caaa6d9668be7ec73c6f6f0cca0e15428ed5e61b
Shuvam Adhikari <shuvamlim234@gmail.com>
2023-08-10 17:52:54 +0545

    Qr codde

---
+++ exercise/indexx.js
@@ -0,0 +1,39 @@
+const QRCode = require('qrcode');
+const data = "https://www.instagram.com/siennaax____/"
+
+QRCode.toString(data,{type:'terminal'}, function (err, url) {
+    console.log(url)
+  })
+  
+                                                              
+//Weather API using package axios and open source weather url LINK
+
+                                 
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                        
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+                                                              
+
