989e096680e4f07537801f3b585b8135cb0b0ce4
Shuvam Adhikari <shuvamlim234@gmail.com>
2023-08-15 18:02:33 +0545

    local host

--- exercise/indexx.js
+++ exercise/indexx.js
@@ -4,50 +4,36 @@
 // QRCode.toString(data,{type:'terminal'}, function (err, url) {
 //     console.log(url)
 //   })
-  
-                                                              
-
 
 //  Write a nodejs application to convert the currency from one currency to another using package: currency-converter-lt
 
 // const CC = require('currency-converter-lt')
 // let currencyConverter = new CC({from:"USD", to:"NPR", amount:100})
 // currencyConverter.convert(100).then((response) => {
-//   console.log(response) 
-// })                                
-                                                              
+//   console.log(response)
+// })
+
 //   Weather API using package axios and open source weather url LINK
-     const axios = required("axios");
-     
-     async function getuser(url){
-      try {
-        const { data } = await axios.get (url);
-        console.log({ data });
-      } catch(error) {
-        console.error(error);
-      }
-     }
- const url = "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
+//      const axios = required("axios");
 
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
-                                                              
+//      async function getuser(url){
+//       try {
+//         const { data } = await axios.get (url);
+//         console.log({ data });
+//       } catch(error) {
+//         console.error(error);
+//       }
+//      }
+//  const url = "https://api.open-meteo.com/v1/forecast?latitude=27.70&longitude=85.32&hourly=temperature_2m&daily=sunrise,sunset&forecast_days=1&timezone=auto"
 
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello world from raktim");
+});
+app.post("/post", function (req, response) {
+  response.send("posted successfully");
+});
+app.listen(3000);
+console.log("App running on port 3000");
--- sendEmail.js
+++ sendEmail.js
@@ -4,29 +4,29 @@
 3.email template ( designed template)
 
 */
-const nodemailer = require ("nodemailer");
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: 'shuvamlim234@gmail.com',
-      pass: ''
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: "shuvamlim234@gmail.com",
+    pass: "",
+  },
+});
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Shuvam Adhikar👻" <shuvamlim234@gmail.com>', // sender address
+    to: "shuvamlim234@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    // text: "Hello world?", // plain text body
+    // html: "<b>Hello world?</b>", // html body
+    template,
   });
-  async function main() {
-    // send mail with defined transport object
-    const info = await transporter.sendMail({
-      from: '"Shuvam Adhikar👻" <shuvamlim234@gmail.com>', // sender address
-      to: "shuvamlim234@gmail.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      // text: "Hello world?", // plain text body
-      // html: "<b>Hello world?</b>", // html body 
-      template 
-    });
-  
-    console.log("Message sent: %s", info.messageId);
+
+  console.log("Message sent: %s", info.messageId);
 }
-    main().catch(console.error);
+main().catch(console.error);
